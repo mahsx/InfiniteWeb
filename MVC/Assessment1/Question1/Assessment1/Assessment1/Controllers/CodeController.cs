02a405d360bca595ebc3b7f2728e3387b7e12e73
mahsx <maheshsek@infinite.com>
2024-04-29 13:16:40 +0530

    Assessment 1 - MVC / 11:56:34 AM

---
+++ MVC/Assessment1/Question1/Assessment1/Assessment1/Controllers/CodeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Assessment1.Models; 
+
+public class CodeController : Controller
+{
+    private NorthWindEntities1 db = new NorthWindEntities1(); 
+
+    public ActionResult GermanyCustomers()
+    {
+        var germanyCustomers = db.Customers.Where(c => c.Country == "Germany").ToList();
+        return View(germanyCustomers);
+    }
+
+    // Action method to return customer details with orderId == 10248
+    public ActionResult CustomerDetailsWithOrder()
+    {
+        var customerWithOrder = db.Customers
+            .Where(c => c.Orders.Any(o => o.OrderID == 10248))
+            .FirstOrDefault();
+        return View(customerWithOrder);
+    }
+}
---
+++ MVC/Assessment1/Question2/Ques2/Ques2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ques2.MvcApplication" Language="C#" %>
