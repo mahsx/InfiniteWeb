503b74e6255899f8eb08c7923b33faa43e12fd0c
mahsx <maheshsek@infinite.com>
2024-04-29 14:17:21 +0530

    Assessment 1 - MVC / Alternate Approch

---
+++ MVC/Assessment1/Question2/Alternate Approch/AssessmentQ2/AssessmentQ2/Data/MoviesDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using AssessmentQ2.Models;
+
+namespace AssessmentQ2.Data
+{
+    public class MoviesDbContext : DbContext
+    {
+        public MoviesDbContext() : base("MoviesDbContext")
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
---
+++ MVC/Assessment1/Question2/Alternate Approch/AssessmentQ2/AssessmentQ2/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AssessmentQ2.Models
+{
+    public class Movie
+    {
+        [Key] // Define Mid as the primary key
+        public int Mid { get; set; }
+
+        [Required(ErrorMessage = "Please enter the movie name.")]
+        public string Moviename { get; set; }
+
+        [Display(Name = "Date of Release")]
+        [DataType(DataType.Date)]
+        public DateTime DateofRelease { get; set; }
+    }
+}
